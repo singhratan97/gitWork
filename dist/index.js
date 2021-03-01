3445e6cc0d7b46448979b74e87ed06f23d86dee5
RATAN Singh <ratansingh@in.ibm.com>
2021-02-28 17:45:27 -0800

    webpack done

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var person_1 = require("./person");
-var obj = { userName: 'ratan', Email: 'ratan@email.com', Color: 'red' };
-var p = new person_1.Person(obj);
-p.getUsername();
-p.getEmail();
-p.getColor();
-//modules means take interface in separate file and use it
